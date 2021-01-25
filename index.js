7ec1e237f68eb6f6212a5ae8841fc186a38880bb
Pwako11 <akolo@hotmail.com>
2021-01-25 17:36:15 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-']+/g, '');
+  }
+
+  static titleize(string){
+
+    // var str = string.split(' ');
+
+    // str.each(str, function(index, strg){
+    //   str[index] = strg.chartAt(0).toUpperCase() + strg.slice(1); 
+    // });
+    // return str.join(' ');
+  }
 }
